4483d2c9a277e7458d57e7123b7ea93d65877418
Sanketh <sankethlgowda@gmail.com>
2024-02-01 22:22:54 +0530

    adding java code to solve the remove element leetcode question

---
+++ javaleetcode/removeelement.java
@@ -0,0 +1,15 @@
+class removeelement{
+public int removeDuplicates(int[] nums) {
+        int x = 0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]!=nums[x]){
+                x++;
+                int a=nums[i];
+                nums[i]=nums[x];
+                nums[x]=a;
+            }
+        }
+        return x+1;
+    }
+}
+
